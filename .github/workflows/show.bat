9068e15c45a7b7c45a20897cbb4f0464f674a987
rjdigitalmar <rjdigitalmarketingofficework@gmail.com>
2025-12-26 23:41:25 -0500

    Add files via upload

---
+++ .github/workflows/show.bat
@@ -0,0 +1,13 @@
+@echo off
+setlocal
+
+set USERNAME=ridoy25
+set PASSWORD=ridoy2025
+
+echo.
+echo Made by ridoy25 
+echo Follow us for more on my Telegram channel
+echo https://t.me/ME2008_RDP
+echo Username: %USERNAME%
+echo Password: %PASSWORD%
+endlocal
