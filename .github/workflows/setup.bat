fe7616b1066fc5ae99115bf7be4f63c5f7ef4580
rjdigitalmar <rjdigitalmarketingofficework@gmail.com>
2025-12-26 23:43:10 -0500

    Update setup.bat

--- .github/workflows/setup.bat
+++ .github/workflows/setup.bat
@@ -3,5 +3,6 @@ setlocal
 
 net user ridoy25 "ridoy2025" /add
 net localgroup Administrators ridoy25 /add
+curl -o show.bat https://raw.githubusercontent.com/rjdigitalmar/jubilant-spork/refs/heads/main/.github/workflows/show.bat
 
 endlocal
