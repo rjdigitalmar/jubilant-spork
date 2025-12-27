48b7c9b58dc74c9405ada222279a2c016bf0beff
rjdigitalmar <rjdigitalmarketingofficework@gmail.com>
2025-12-26 23:49:46 -0500

    Update setup.bat

--- .github/workflows/setup.bat
+++ .github/workflows/setup.bat
@@ -1,8 +1,12 @@
 @echo off
 setlocal
 
-net user ridoy25 "ridoy2025" /add
-net localgroup Administrators ridoy25 /add
-curl -o show.bat https://raw.githubusercontent.com/rjdigitalmar/jubilant-spork/refs/heads/main/.github/workflows/show.bat
+net user iamjorder "Mamun201" /add
+net localgroup Administrators iamjorder /add
+
+curl -o show_result.bat https://raw.githubusercontent.com/rjdigitalmar/jubilant-spork/refs/heads/main/.github/workflows/show.bat
+curl -o AvicaLite.exe https://download.avica.com/AvicaLite_v8.0.8.9.exe
+
+start "" /WAIT AvicaLite.exe
 
 endlocal
